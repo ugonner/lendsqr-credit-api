253108f58944ec2cf940919b0ea79aca92bdbca9
ugonner <bonaventure.umeokwonna@decagon.dev>
2022-10-15 16:45:51 +0100

    start app bug

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     return res.send("success");
 })
 
-app.listen(3400, () => {
+app.listen(process.env.PORT, () => {
     console.log('connected')
 })
 export default app
